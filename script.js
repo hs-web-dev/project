f951522ab57face6119e5b0ae84f4f43d35b6ddb
hs-web-dev <hadr.slno@gmail.com>
2026-05-09 15:29:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".login-btn").addEventListener("click", () => {
   alert("Connexion cliquée");
 });
+
+document.querySelector(".cta-btn").addEventListener("click", () => {
+  window.location.href = "#projets";
+});

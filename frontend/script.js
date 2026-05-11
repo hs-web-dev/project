6fa5dcab2046ee1ac43c9ed282befa20f1641d2b
hs-web-dev <hadr.slno@gmail.com>
2026-05-11 22:56:18 +0200

    d

--- frontend/script.js
+++ frontend/script.js
@@ -126,7 +126,7 @@ document.getElementById("register-submit").onclick = async () => {
     return;
   }
 
-  const res = await fetch("https://project-nqj7.onrender.com", {
+  const res = await fetch("https://project-nqj7.onrender.com/auth/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
